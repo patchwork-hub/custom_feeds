98e048c1ca9b47e4eda683712b1f21e7b3a038b0
NangKhatMang <nkm.nangkhatmang@gmail.com>
2026-01-13 18:02:30 +0630

    add custom timeline for mobile

---
+++ app/models/custom_feeds/concerns/status_concern.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+module CustomFeeds::Concerns::StatusConcern
+  extend ActiveSupport::Concern
+
+  included do
+    after_create :add_status_to_mix_channel_local_timeline
+    after_destroy :remove_status_from_mix_channel_local_timeline
+  end
+
+  private
+
+  def add_status_to_mix_channel_local_timeline
+    if self.local?
+      CustomFeeds::CustomTimelineService.new.add_custom_public_status(self.id)
+    end
+  end
+
+  def remove_status_from_mix_channel_local_timeline
+    if self.local?
+      CustomFeeds::CustomTimelineService.new.remove_custom_public_status(self.id)
+    end
+  end
+end
--- config/initializers/prepend_concerns.rb
+++ config/initializers/prepend_concerns.rb
@@ -1,4 +1,5 @@
 Rails.application.config.to_prepare do
+  Status.include(CustomFeeds::Concerns::StatusConcern)
   FeedManager.include(CustomFeeds::Concerns::FeedManagerConcern)
   FanOutOnWriteService.include(CustomFeeds::Concerns::FanOutOnWriteConcern)
   RemoveStatusService.prepend(Overrides::RemoveStatusServiceExtension)
