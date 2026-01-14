8bdc3237c367bb959027b74021324605b35a436b
Nyan Lin Htut <nyanlinhtut925@gmail.com>
2026-01-14 11:26:27 +0700

    feat: add for you custom timeline

---
+++ app/controllers/custom_feeds/api/v1/custom_statuses/custom_boost_bot_status_controller.rb
@@ -0,0 +1,83 @@
+module CustomFeeds::Api::V1::CustomStatuses
+  class CustomBoostBotStatusController < Api::BaseController
+    include Redisable
+
+    before_action :require_auth!
+
+    RESULTS_LIMIT = 20
+    
+    def add_custom_boost_bot_status
+      @status_url = params[:status_url]
+      return render json: { error: "Status URL is required" }, status: :bad_request unless @status_url.present?
+
+      statuses = search_results
+      
+      if statuses[:statuses].present?
+        statuses[:statuses].each do |status|
+          CustomFeeds::CustomTimelineService.new.add_custom_public_status(status.id)
+        end
+        render json: statuses
+      else
+        CustomFeeds::CustomTimelineService.new.add_custom_public_status(1234567896)
+        render json: { error: "No status found" }, status: :not_found
+      end
+    end
+
+    def remove_custom_boost_bot_status
+      status_id = params[:status_id]
+      return render json: { error: "Status ID is required" }, status: :bad_request unless status_id.present?
+
+      # status = Status.find(status_id)
+      # return render json: { error: "Status not found" }, status: :not_found unless status.present?
+      CustomFeeds::CustomTimelineService.new.remove_custom_public_status(status_id)
+      render json: { message: "Status removed from custom boost bot timeline" } 
+    end
+
+    private
+
+    def search_results
+      SearchService.new.call(
+        @status_url,
+        current_account,
+        limit_param(RESULTS_LIMIT),
+        combined_search_params
+      )
+    end
+
+    def combined_search_params
+      search_params.merge(
+        resolve: truthy_param?(:resolve),
+        exclude_unreviewed: truthy_param?(:exclude_unreviewed),
+        following: truthy_param?(:following),
+        query_fasp: @query_fasp
+      )
+    end
+  
+    def search_params
+      params.permit(
+        :type,
+        :offset,
+        :min_id,
+        :max_id,
+        :account_id,
+        :following,
+        :status_url,
+        :format,
+        :client_id,
+        :client_secret
+      )
+    end
+
+    def require_auth!
+      return render json: { error: "Client ID and client secret are required" }, status: :bad_request unless params[:client_id].present? && params[:client_secret].present?
+
+      app = Doorkeeper::Application.find_by_uid(params[:client_id])
+      return render json: { error: "Invalid client ID or client secret" }, status: :unauthorized unless app.present?
+
+      client_id = app&.uid
+      client_secret = app&.secret
+
+      render json: { error: "Invalid client ID or client secret" }, status: :unauthorized unless client_id == params[:client_id] && client_secret == params[:client_secret]
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ CustomFeeds::Engine.routes.draw do
     namespace :v1 do
       namespace :timelines do
         get "@:username/feed", to: "feeds#show", as: :custom_feed
+        get "for_you_custom_timeline", to: "for_you_custom_timeline#show", as: :for_you_custom_timeline
+      end
+      namespace :custom_statuses do
+        post "add_custom_boost_bot_status", to: "custom_boost_bot_status#add_custom_boost_bot_status", as: :add_custom_boost_bot_status
+        post "remove_custom_boost_bot_status", to: "custom_boost_bot_status#remove_custom_boost_bot_status", as: :remove_custom_boost_bot_status
       end
     end
   end
