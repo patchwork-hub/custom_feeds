44b89c3d9f9e0a87b9fb896dd176ff9f56271675
NangKhatMang <nkm.nangkhatmang@gmail.com>
2026-01-15 17:08:18 +0630

    update: env filter condition

--- app/models/custom_feeds/concerns/status_concern.rb
+++ app/models/custom_feeds/concerns/status_concern.rb
@@ -4,7 +4,7 @@ module CustomFeeds::Concerns::StatusConcern
   extend ActiveSupport::Concern
 
   included do
-    if ENV['FOR_YOU_TIMELINE_ENABLED']&.casecmp?('true')
+    if ENV['FOR_YOU_TIMELINE_ENABLED'].present? && ENV['FOR_YOU_TIMELINE_ENABLED'].to_s.downcase == 'true'
       after_create :add_status_to_mix_channel_local_timeline
       after_destroy :remove_status_from_mix_channel_local_timeline
     end
