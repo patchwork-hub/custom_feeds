8ac99c12d2132ff8fdb38d5e3dc0daa5859e097d
NangKhatMang <nkm.nangkhatmang@gmail.com>
2026-01-16 11:30:41 +0630

    add: staging testing log

--- app/models/custom_feeds/concerns/status_concern.rb
+++ app/models/custom_feeds/concerns/status_concern.rb
@@ -6,7 +6,8 @@ module CustomFeeds::Concerns::StatusConcern
   included do
     Rails.logger.info '>>>> Start staging testing log : StatusConcern'
     Rails.logger.info "check env FOR_YOU_TIMELINE_ENABLED : #{ENV['FOR_YOU_TIMELINE_ENABLED']}"
-    if ENV['FOR_YOU_TIMELINE_ENABLED'].present? && ENV['FOR_YOU_TIMELINE_ENABLED'].to_s.downcase == 'true'
+    if ENV['FOR_YOU_TIMELINE_ENABLED'].present? && ENV['FOR_YOU_TIMELINE_ENABLED']
+      Rails.logger.info ">>>> staging test env FOR_YOU_TIMELINE_ENABLED : check after_create and after_destroy"
       after_create :add_status_to_mix_channel_local_timeline
       after_destroy :remove_status_from_mix_channel_local_timeline
     end
