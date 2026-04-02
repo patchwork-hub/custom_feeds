7844702602e6ff1f1d9258a9a39062cf2830a3a9
Min Khant Kyaw <minkhantkyawmdy35@gmail.com>
2026-04-02 10:42:25 +0630

    Update for_you_feed.rb

--- app/models/custom_feeds/for_you_feed.rb
+++ app/models/custom_feeds/for_you_feed.rb
@@ -30,6 +30,7 @@ class CustomFeeds::ForYouFeed
     scope.merge!(account_filters_scope) if account?
     scope.merge!(media_only_scope) if media_only?
     scope.merge!(grouped_admin_statuses_scope) if grouped_admin_statuses?
+    scope.merge!(grouped_admin_reblogged_statuses_scope) if grouped_admin_statuses?
     scope.merge!(language_scope) if account&.chosen_languages.present?
 
     scope.to_a_paginated_by_id(limit, max_id: max_id, since_id: since_id, min_id: min_id)
@@ -101,12 +102,18 @@ class CustomFeeds::ForYouFeed
   end
 
   def grouped_admin_statuses?
-    options[:grouped_admin_statuses]
+    options[:grouped_admin_statuses] && Status.column_names.include?('local_only')
   end
 
   def grouped_admin_statuses_scope
     grouped_admin_account_ids = fetch_grouped_admin_account_ids
-    Status.where.not(account_id: grouped_admin_account_ids) if grouped_admin_account_ids.any?
+    Status.where.not(account_id: grouped_admin_account_ids, local_only: true, local: true)
+  end
+
+  def grouped_admin_reblogged_statuses_scope
+    grouped_admin_account_ids = fetch_grouped_admin_account_ids
+    grouped_admin_reblogged_ids = Status.where(account_id: grouped_admin_account_ids, local_only: true, local: true).pluck(:reblog_of_id).compact
+    Status.where.not(id: grouped_admin_reblogged_ids)
   end
 
   def fetch_grouped_admin_account_ids
